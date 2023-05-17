74c911e860038520bef63ecf2e2cb0157300909c
tutruong193 <128039200+tutruong193@users.noreply.github.com>
2023-05-17 16:04:06 +0700

    ok

--- lab04/web02/routes/index.js
+++ lab04/web02/routes/index.js
@@ -5,5 +5,23 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//render ra form input
+router.get('/input', function(req, res){
+  res.render('input');
+});
+//lay du lieu tu input
+router.post('/output', function(req, res){
+  var student = req.body;
+  var tuoi = req.query.tuoi;
+  var ten = req.query.ten;
+  var email = req.query.email;
+  var ngaysinh = req.query.ngaysinh;
+  //in ra consolelog
+  // console.log(req.body);
+  // //in ra web voi res.send
+  // res.send(req.body);
+  //render ra trong output kem du lieu tu form
+  res.render('output',{sinhvien : student});
+});
 
 module.exports = router;
