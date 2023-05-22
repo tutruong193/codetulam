3812dab1b262f195dc8797bdf94a535fd5d56550
tutruong193 <128039200+tutruong193@users.noreply.github.com>
2023-05-22 14:01:59 +0700

    ok

--- lab04/web02/routes/index.js
+++ lab04/web02/routes/index.js
@@ -2,20 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 //render ra form input
-router.get('/input', function(req, res){
+router.get('/', function(req, res){
   res.render('input');
 });
 //lay du lieu tu input
 router.post('/output', function(req, res){
   var student = req.body;
-  var tuoi = req.query.tuoi;
-  var ten = req.query.ten;
-  var email = req.query.email;
-  var ngaysinh = req.query.ngaysinh;
+  // var tuoi = req.query.tuoi;
+  // var ten = req.query.ten;
+  // var email = req.query.email;
+  // var ngaysinh = req.query.ngaysinh;
   //in ra consolelog
   // console.log(req.body);
   // //in ra web voi res.send
@@ -24,5 +22,19 @@ router.post('/output', function(req, res){
   res.render('output',{sinhvien : student});
   //c2res.render('output',{name: ten, email : email, age : tuoi, bod: ngaysinh});
 });
+
+router.get('/login', function(req, res){
+  res.render('login');
+})
+router.post('/check', function(req, res){
+    var account = req.body;
+    if (account.username == 'admin' && account.password == 'admin'){
+      res.render('check',{title: "Successfully"});
+      res.send("<h1>Ok</h1>")
+    } else {
+      res.render('check',{title: "Failed"})
+      res.send("<h1>Error</h1>")
+    };
+})
 
 module.exports = router;
